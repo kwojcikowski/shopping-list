c601fe0fc1583f7923baf0feaa5b1a3080534784
Kacper Wojcikowski <wojcikowskikacper5@gmail.com>
2021-02-27 20:10:46 +0100

    Implemented website header with styling

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,29 +1,47 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import "./HeaderStyle.css";
+import {Nav, Navbar, NavItem} from "react-bootstrap";
 
 const Header = () => {
-  const activeStyle = { color: "#F15B2A" };
-  return (
-    <>
-      <nav style={{ display: "inline-block" }}>
-        <NavLink to="/" activeStyle={activeStyle} exact>
-          Lista zakupów
-        </NavLink>
-        {" | "}
-        <NavLink to={"/products"} activeStyle={activeStyle}>
-          Produkty
-        </NavLink>
-        {" | "}
-        <NavLink to="/recipes" activeStyle={activeStyle}>
-          Przepisy
-        </NavLink>
-        {" | "}
-        <NavLink to="/stores" activeStyle={activeStyle}>
-          Sklepy
-        </NavLink>
-      </nav>
-    </>
-  );
+    const activeStyle = {color: "white"};
+
+    return (
+        <Navbar bg="success" variant="dark" sticky="top">
+            <Navbar.Brand as={NavLink} to="/" >Shopping List</Navbar.Brand>
+            <Navbar.Toggle/>
+            <Navbar.Collapse>
+                <Nav className="mr-auto nav-text">
+                    <NavItem href="/">
+                        <Nav.Link as={NavLink} to="/" activeStyle={activeStyle} exact>
+                            Lista zakupów
+                        </Nav.Link>
+                    </NavItem>
+                    <NavItem href="/products">
+                        <Nav.Link as={NavLink} to="/products" activeStyle={activeStyle}>
+                            Produkty
+                        </Nav.Link>
+                    </NavItem>
+                    <NavItem href="/recipes">
+                        <Nav.Link as={NavLink} to="/recipes" activeStyle={activeStyle}>
+                            Przepisy
+                        </Nav.Link>
+                    </NavItem>
+                    <NavItem href="/stores">
+                        <Nav.Link as={NavLink} to="/stores" activeStyle={activeStyle}>
+                            Sklepy
+                        </Nav.Link>
+                    </NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        // <div className="header-wrapper">
+        //   <div className="logo-wrapper">
+        //     <h2>Shopping List</h2>
+        //   </div>
+        //   <div></div>
+        // </div>
+    );
 };
 
 export default Header;
